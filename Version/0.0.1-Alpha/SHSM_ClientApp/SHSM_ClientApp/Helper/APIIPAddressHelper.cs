fa345d11c70243c2ab8311e48de720163baa51c1
Chrono <chrono_legionnaire@protonmail.com>
2026-07-15 15:05:51 +0800

    Add files via upload

---
+++ Version/0.0.1-Alpha/SHSM_ClientApp/SHSM_ClientApp/Helper/APIIPAddressHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHSM_ClientApp.Helper
+{
+    public static class APIIPAddressHelper
+    {
+        public static String? IPAddress { get; set; }
+
+        public static Boolean HasSet { get; set; }
+    }
+}
---
+++ Version/0.0.1-Alpha/SHSM_ClientApp/SHSM_ClientApp/Helper/CryptographicSecureIDGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using ASodium;
+
+namespace SHSM_ClientApp.Helper
+{
+    public static class CryptographicSecureIDGenerator
+    {
+        public static String GenerateUniqueString()
+        {
+            Byte[] Random_Data = SodiumRNG.GetRandomBytes(128);
+            int Loop = 0;
+            StringBuilder stringBuilder = new StringBuilder();
+            while (Loop < Random_Data.Length)
+            {
+                if (Random_Data[Loop] >= 48 && Random_Data[Loop] <= 57)
+                {
+                    stringBuilder.Append((char)Random_Data[Loop]);
+                }
+                else if (Random_Data[Loop] >= 65 && Random_Data[Loop] <= 90)
+                {
+                    stringBuilder.Append((char)Random_Data[Loop]);
+                }
+                else if (Random_Data[Loop] >= 97 && Random_Data[Loop] <= 122)
+                {
+                    stringBuilder.Append((char)Random_Data[Loop]);
+                }
+                Loop += 1;
+            }
+            if (stringBuilder.ToString().CompareTo("") != 0)
+            {
+                return stringBuilder.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public static String GenerateMinimumAmountOfUniqueString(int Amount)
+        {
+            String TestString = GenerateUniqueString();
+            while (TestString.Length < Amount)
+            {
+                TestString += GenerateUniqueString();
+            }
+            return TestString;
+        }
+    }
+}
