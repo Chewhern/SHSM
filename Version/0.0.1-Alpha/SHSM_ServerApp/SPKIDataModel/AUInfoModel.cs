df91d3591376fc9e64a3e9c2f114b1391eecaece
Chrono <chrono_legionnaire@protonmail.com>
2026-07-15 14:46:38 +0800

    Add files via upload

---
+++ Version/0.0.1-Alpha/SHSM_ServerApp/SPKIDataModel/AUInfoModel.cs
@@ -0,0 +1,25 @@
+﻿namespace SHSM_ServerApp.SPKIDataModel
+{
+    public class AUInfoModel
+    {
+        public String User_ID { get; set; }
+
+        public String Public_Contact { get; set; }
+
+        public String Private_Contact { get; set; }
+
+        public String Sign_PK { get; set; }
+
+        public String Auth_PK { get; set; }
+
+        public String OOB_PK { get; set; }
+
+        public String DSA_Type { get; set; }
+
+        public int ValidDate_Day { get; set; }
+
+        public int ValidDate_Month { get; set; }
+
+        public int ValidDate_Year { get; set; }
+    }
+}
---
+++ Version/0.0.1-Alpha/SHSM_ServerApp/SPKIDataModel/SubSignedPKModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SHSM_ServerApp.SPKIDataModel
+{
+    public class SubSignedPKModel
+    {
+        public String SignedDigitalSignaturePublicKeyB64 { get; set; }
+
+        public String DigitalSignatureAlgorithm { get; set; }
+
+        public int ValidDate_Day { get; set; }
+
+        public int ValidDate_Month { get; set; }
+
+        public int ValidDate_Year { get; set; }
+    }
+}
