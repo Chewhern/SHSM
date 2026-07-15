59d918efa591bf08ca62c3541ca981a92059169e
Chrono <chrono_legionnaire@protonmail.com>
2026-07-15 14:51:33 +0800

    Add files via upload

---
+++ Version/0.0.1-Alpha/SHSM_ServerApp/ActualSHSM/ServerSHSMModel.cs
@@ -0,0 +1,13 @@
+﻿using ASodium;
+
+namespace SHSM_ServerApp.ActualSHSM
+{
+    public class ServerSHSMModel
+    {
+        public KeyPair MACX25519KeyPair { get; set; }
+
+        public KeyPair EncryptionX25519KeyPair { get; set; }
+
+        public KeyPair MyKEMKeyPair { get; set; }
+    }
+}
---
+++ Version/0.0.1-Alpha/SHSM_ServerApp/ActualSHSM/UserSHSMModel.cs
@@ -0,0 +1,36 @@
+﻿using ASodium;
+using BCASodium;
+using SHSM_ServerApp.RSACredentials;
+using SHSM_ServerApp.SHSMDataModel;
+
+namespace SHSM_ServerApp.ActualSHSM
+{
+    public class UserSHSMModel
+    {
+        public RegisteredUserModel RegisteredUser { get; set; }
+
+        public IntPtr SecretKeyForEncryption { get; set; }
+
+        public IntPtr SecretKeyForMAC { get; set; }
+
+        public KeyPair ED25519DigitalSignatureKP { get; set; }
+
+        public IntPtr ED255119SecretKey { get; set; }
+
+        public ED448RevampedKeyPair ED448DigitalSignatureRKP { get; set; }
+        //Unless specifically work on some parts from bouncycastle
+        //else can't prevent it from ended up in swap partitions..
+
+        public Byte[] ED448SecretKey { get; set; }
+
+        public RSACredentialsModel RSAKeyParts { get; set; }
+
+        public IntPtr X25519EncryptionSecretKey { get; set; }
+
+        public IntPtr EncryptionKEMKPSecretKey { get; set; }
+
+        public DateTime ValidDateTime { get; set; }
+
+        public ServerSHSMModel TempServerSHSM { get; set; }
+    }
+}
